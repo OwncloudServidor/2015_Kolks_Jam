26c299af48e97d797bcd63ff6e74bec81bd60281
8Nove <gaabrielmooraes@gmail.com>
2015-04-25 11:13:54 -0300

    Tudo passado

---
+++ SOURCE/Kolks_Jam_Agua/Assets/Scripts/Morte.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Morte : MonoBehaviour 
+{
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			other.GetComponent<Player>().Morrer();
+		}
+	}
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ SOURCE/Kolks_Jam_Agua/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public GameObject posicaoInicio;
+
+	public void Morrer()
+	{
+		transform.position = posicaoInicio.transform.position;
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		transform.position = posicaoInicio.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
